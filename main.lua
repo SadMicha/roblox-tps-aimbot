e06f8a54b59731edf008686019ab33fd5a9e0449
SadMicha <55989084+SadMicha@users.noreply.github.com>
2022-09-06 21:55:23 +0200

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,3 +1,19 @@
+-- options
+getgenv().options = {
+    aimbot = true,
+    aimbot_toggle_key = "NONE",
+    aimbot_key = "NONE",
+    fov = 300,
+    show_fov = true,
+    fov_color = Color3.new(0, 0, 0),
+    smoothness = 1,
+    triggerbot = false,
+
+    team_check = true,
+    wall_check = true
+}
+
+-- ui stuff
 local Bracket = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexR32/Bracket/main/BracketV32.lua"))()
 
 local Window = Bracket:Window({Name = "vakware but better", Enabled = true, Color = Color3.new(1,0.5,0.25), Size = UDim2.new(0,496,0,496), Position = UDim2.new(0.5,-248,0.5,-248)}) do
@@ -5,51 +21,52 @@ local Window = Bracket:Window({Name = "vakware but better", Enabled = true, Colo
         -- settings
         Aimbot:Divider({Text = "Settings", Side = "Left"})
         local SettingSection = Aimbot:Section({Name = "Settings", Side = "Left"}) do
-            SettingSection:Toggle({Name = "Aimbot", Value = true, Callback = function(bool)
-                print(bool)
-            end}):Keybind({Key = "NONE", Mouse = false, Blacklist = {"W","A","S","D","Slash","Tab","Backspace","Escape","Space","Delete","Unknown","Backquote"}, Callback = function(bool, key)
-                print(bool, key)
+            SettingSection:Toggle({Name = "Aimbot", Value = options.aimbot, Callback = function(bool)
+                options.aimbot = bool
+            end}):Keybind({Key = options.aimbot_toggle_key, Mouse = false, Blacklist = {"W","A","S","D","Slash","Tab","Backspace","Escape","Space","Delete","Unknown","Backquote"}, Callback = function(bool, key)
+                options.aimbot_toggle_key = bool
             end})
             
-            SettingSection:Keybind({Name = "Aimbot Key", Key = "NONE", Mouse = true, Blacklist = {"W","A","S","D","Slash","Tab","Backspace","Escape","Space","Delete","Unknown","Backquote"}, Callback = function(bool, key)
-                print(bool, key)
+            SettingSection:Keybind({Name = "Aimbot Key", Key = options.aimbot_key, Mouse = true, Blacklist = {"W","A","S","D","Slash","Tab","Backspace","Escape","Space","Delete","Unknown","Backquote"}, Callback = function(bool, key)
+                options.aimbot_key = bool
             end})
 
-            SettingSection:Slider({Name = "Smoothness", Min = 0, Max = 10, Value = 0, Precise = 1, Unit = "", Callback = function(number)
-                print(number)
+            SettingSection:Slider({Name = "Smoothness", Min = 0, Max = 10, Value = options.smoothness, Precise = 1, Unit = "", Callback = function(number)
+                options.smoothness = number
             end})
 
-            SettingSection:Toggle({Name = "Triggerbot", Value = false, Callback = function(bool)
-                print(bool)
+            SettingSection:Toggle({Name = "Triggerbot", Value = options.triggerbot, Callback = function(bool)
+                options.triggerbot = bool
             end})
         end
 
         -- visual
         Aimbot:Divider({Text = "Visual", Side = "Left"})
         local Visual = Aimbot:Section({Name = "Visual", Side = "Left"}) do
-            Visual:Slider({Name = "FOV", Min = 0, Max = 1000, Value = 0, Precise = 2, Unit = "", Callback = function(number)
-                print(number)
+            Visual:Slider({Name = "FOV", Min = 0, Max = 1000, Value = options.fov, Precise = 2, Unit = "", Callback = function(number)
+                options.fov = number
             end})
 
-            Visual:Toggle({Name = "Show FOV", Value = true, Callback = function(bool)
-                print(bool)
+            Visual:Toggle({Name = "Show FOV", Value = options.show_fov, Callback = function(bool)
+                options.show_fov = bool
             end})
 
-            Visual:Colorpicker({Name = "FOV Color", Color = Color3.new(1,0,0), Callback = function(color,table)
-                print(color, table)
+            Visual:Colorpicker({Name = "FOV Color", Color = options.fov_color, Callback = function(color,table)
+                options.fov_color = color
             end})
         end
 
         -- misc
         Aimbot:Divider({Text = "Misc", Side = "Right"})
         local Misc = Aimbot:Section({Name = "Misc", Side = "Right"}) do
-            Misc:Toggle({Name = "Team Check", Value = true, Callback = function(bool)
-                print(bool)
+            Misc:Toggle({Name = "Team Check", Value = options.team_check, Callback = function(bool)
+                options.team_check = bool
             end})
 
-            Misc:Toggle({Name = "Wall Check", Value = true, Callback = function(bool)
-                print(bool)
+            Misc:Toggle({Name = "Wall Check", Value = options.wall_check, Callback = function(bool)
+                options.wall_check = bool
             end})
         end
     end
 end
+
