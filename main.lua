940747e6279bc80322517886004433944f4724b8
SadMicha <55989084+SadMicha@users.noreply.github.com>
2022-09-08 21:09:05 +0200

    Add files via upload

--- main.lua
+++ main.lua
@@ -487,8 +487,8 @@ local function stepped()
                     if start_aim then
                         local smoothness = options.smoothness
                         if chosen.visible then
-                            local x = (chosen.screen.X - mouse.X) + math.random(10, 20) / (smoothness * 2)
-                            local y = (chosen.screen.Y - (mouse.Y + 36)) + math.random(10, 20) / (smoothness * 2)
+                            local x = ((chosen.screen.X - mouse.X) + math.random(10, 20)) / (smoothness * 2)
+                            local y = ((chosen.screen.Y - (mouse.Y + 36)) + math.random(10, 20)) / (smoothness * 2)
                             mousemoverel(x, y)
                         end
                     end
